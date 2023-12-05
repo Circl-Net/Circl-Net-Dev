59d36bf12844d2930b0d802b2e4dc090de71699a
Tongxin Sun <Sunny201410@outlook.com>
2023-12-05 14:11:54 -0800

    -make changes to the login page and its styling

--- circle/src/pages/Login.js
+++ circle/src/pages/Login.js
@@ -1,38 +1,93 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
- return (
-     <>
-     <div class="circle">
+    const [password, setPassword] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [usernameOrEmailOrPhone, setUsernameOrEmailOrPhone] = useState("");
 
-    <form action="/login" method="POST" id="login">
+    const navigate = useNavigate();
+
     
-        <label for="username" class="reqired"> 
-        <input type="text" name="username" id="username" 
-        required
-        pattern="(\d.{9})|([A-Za-z\d])|([^ @]+@[^ @]+.[a-z]+)"
-        placeholder="Username, email or phone number " />
-        </label>
+    const user = {
+    email: "test@test.com",
+    username: "test123",
+    password: "123456"
+    };
 
-        <label for="password" class="required"></label>
-        <input type="password" 
-        name="password" 
-        id="password"
-        required
-        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-        placeholder="Password" /> 
-        <div class="flex-container" id="not-login">
-        <Link to="../signup">Not a Circler? Sign up</Link>
-       
-       <a href="">Forgot password</a>
-       </div>
-        <button type="submit">Log in</button>
-      
-    </form>
-  </div>
-     </>
- )   
+    const onButtonClick = () => {
+        
+        if ("" === usernameOrEmailOrPhone) {
+            setEmailError("Please enter your email or username or phone number.")
+            return
+        }
+
+        if ("" === password) {
+            setPasswordError("Please enter a password")
+            return
+        }
+
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(usernameOrEmailOrPhone)) {
+            setEmailError("Please enter a valid email")
+            return
+        }
+
+        if (password.length < 7) {
+            setPasswordError("The password must be 8 characters or longer")
+            return
+        }
+
+    }
+
+    return (
+        <div className="circle">
+            <div className="login-title">
+                <div>Login</div>
+            </div>
+            <br />
+            <form action="/login" method="POST" id="login">
+                <div className='inputContainer'><p className="errorLabel">{emailError}</p>
+                <label htmlFor="usernameOrEmailOrPhone" className="required"></label>
+                <input
+                    type="text"
+                    name="usernameOrEmailOrPhone"
+                    id="usernameOrEmailOrPhone"
+                    value={usernameOrEmailOrPhone}
+                    onChange={(e) => setUsernameOrEmailOrPhone(e.target.value)}
+                    required
+                    pattern="(\d.{9})|([A-Za-z\d])|([^ @]+@[^ @]+\.[a-z]+)"
+                    placeholder="Username, email, or phone number"
+                /></div>
+                
+                <div className='inputContainer'><p className="errorLabel">{passwordError}</p>
+                <label htmlFor="password" className="required"></label>
+                <input
+                    type="password"
+                    value={password}
+                    name="password"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    placeholder="Password"
+                /></div>
+                
+
+                <div className="flex-container" id="not-login">
+                    <Link to="../signup">Not a Circler? Sign up</Link>
+                    <a href="">Forgot password</a>
+                </div>
+
+                <input
+                    className="submit"
+                    type="button"
+                    onClick={onButtonClick}
+                    value="Log in"
+                />
+            </form>
+        </div>
+    );
 }
 
 export default Login;
